43bb1385acbfe2f19bb98bc405b9ccacc9b4f298
SanaR511 <72316500+SanaR511@users.noreply.github.com>
2021-04-05 20:43:16 -0400

    Create CalcOp.java

---
+++ Calculator/CalcOp.java
@@ -0,0 +1,7 @@
+interface CalcOp {
+    public int add(int x, int y);
+    public int subtract(int x, int y);
+    public int multiply(int x, int y);
+    public int divide(int x, int y);
+    public int hexToDec(String hexValue);
+}
